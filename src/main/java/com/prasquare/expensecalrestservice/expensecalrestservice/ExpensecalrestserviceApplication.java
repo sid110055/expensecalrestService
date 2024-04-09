ba21f7b44bf4f1a3a3b8fa0ef11329bc0f9dc954
Siddhartha Chikkavarapu <siddu_chikkavarapu@Siddharthas-MacBook-Pro.local>
2024-04-09 17:40:13 -0500

    initial commit

---
+++ src/main/java/com/prasquare/expensecalrestservice/expensecalrestservice/ExpensecalrestserviceApplication.java
@@ -0,0 +1,13 @@
+package com.prasquare.expensecalrestservice.expensecalrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensecalrestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensecalrestserviceApplication.class, args);
+	}
+
+}
